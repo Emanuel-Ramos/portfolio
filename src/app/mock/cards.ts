4330c75d0acddefa886d763b224b8b4f1178c7a8
Emanuel ramos <emanuel@m3ecommerce.com>
2024-01-15 16:25:24 -0300

    feat: add personal content

--- src/app/mock/cards.ts
+++ src/app/mock/cards.ts
@@ -1,4 +1,8 @@
-import ProjectDefault from "@/assets/imgs/default-project.jpg";
+// import ProjectDefault from "@/assets/imgs/default-project.jpg";
+import Larebaja from "@/assets/imgs/Screenshot_1.png";
+import Trocafy from "@/assets/imgs/Screenshot_2.png";
+import Constance from "@/assets/imgs/Screenshot_3.png";
+import Exito from "@/assets/imgs/Screenshot_4.png";
 
 export const skillsCardsMock = [
   { title: "ReactJS", subTitle: "4 Years Experience" },
@@ -8,21 +12,27 @@ export const skillsCardsMock = [
 
 export const projectsCardsMock = [
   {
-    img: ProjectDefault,
-    title: "Front-End",
-    tecnologies: ["HTML", "CSS"],
-    link: "/",
+    img: Exito,
+    title: "Exito",
+    tecnologies: ["NextJS", "NodeJS"],
+    link: "https://www.exito.com/",
   },
   {
-    img: ProjectDefault,
-    title: "Machine Learning",
-    tecnologies: ["HTML", "CSS"],
-    link: "/",
+    img: Larebaja,
+    title: "Larebaja",
+    tecnologies: ["ReactJS", "NodeJS"],
+    link: "https://www.larebajavirtual.com/",
   },
   {
-    img: ProjectDefault,
-    title: "Python",
-    tecnologies: ["HTML", "CSS"],
-    link: "/",
+    img: Constance,
+    title: "Constance",
+    tecnologies: ["ReactJS"],
+    link: "https://www.constance.com.br/",
+  },
+  {
+    img: Trocafy,
+    title: "Trocafy",
+    tecnologies: ["ReactJS", "NodeJS"],
+    link: "https://www.trocafy.com.br/",
   },
 ];
--- src/components/GlobalComponents/Footer/Contact/ContactForm/index.tsx
+++ src/components/GlobalComponents/Footer/Contact/ContactForm/index.tsx
@@ -5,15 +5,25 @@ import "../contact.scss";
 
 const ContactForm = () => {
   return (
-    <form className={classNames("contact-form")}>
-      <input className={classNames("contact-form-input")} placeholder="Name" />
+    <form
+      className={classNames("contact-form")}
+      action="mailto:emanuel.ramos115@hotmail.com"
+      method="post"
+    >
+      <input
+        className={classNames("contact-form-input")}
+        placeholder="Name"
+        name="Name"
+      />
       <input
         className={classNames("contact-form-input")}
         placeholder="E-mail"
+        name="Email"
       />
       <input
         className={classNames("contact-form-input-text")}
         placeholder="MESSAGE"
+        name="Message"
       />
       <button type="submit" className={classNames("contact-form-submit")}>
         SEND MESSAGE
